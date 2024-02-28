6617d96d67ff0da671d1b1a58a540bf1cf3ec3b3
johnafekuana <afekuanajohn@gmail.com>
2024-02-28 11:36:05 +0100

    updated index.js file

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,44 +7,47 @@ import NewTransactionModal from '../components/transaction/NewTransactionModal'
 import TransactionsList from '../components/transaction/TransactionsList'
 import { useWallet } from '@solana/wallet-adapter-react'
 import TransactionQRModal from '../components/transaction/TransactionQRModal'
-import { transactions } from '../data/transactions'
 import { getAvatarUrl } from "../functions/getAvatarUrl"
+import { useSolTip } from '../hooks/soltip';
 
 
 
 const Home = () => {
-    const { connected, publicKey } = useWallet()
-    const [userAddress, setUserAddress] = useState("11111111111111111111111111111111")
-    const [avatar, setAvatar] = useState("")
-    const [transactionQRModalOpen, setTransactionQRModalOpen] = useState(false)
-    const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false)
+const [transactionQRModalOpen, setTransactionQRModalOpen] = useState(false)
 
-    // Get Avatar based on the userAddress
-    useEffect(() => {
-        setAvatar(getAvatarUrl(userAddress))
-    })
+const [qrCode, setQrCode] = useState(false)
 
+const { 
+		connected, 
+		publicKey, 
+		avatar, 
+		userAddress, 
+		transactions , 
+		newTransactionModalOpen, 
+		setNewTransactionModalOpen
+	} = useSolTip()
 
+return (
+	<div className="flex min-h-screen ">
+		<main className="flex flex-1 flex-col">
+			<h1 className='text-center font-bold text-xl font-poppins m-2 text-[#7ac1e0]'>SOL TIP</h1>
+			<SearchBar />
+			<TransactionsList connected={connected} transactions={transactions} />
+		</main>
 
-    return (
-        <div className="flex min-h-screen ">
-            <header className="flex w-[250px] flex-col bg-[#0bb534] p-12">
-                <Profile setModalOpen={setTransactionQRModalOpen} avatar={avatar} userAddress={userAddress} />
-                <TransactionQRModal modalOpen={transactionQRModalOpen} setModalOpen={setTransactionQRModalOpen} userAddress={userAddress} myKey={publicKey} />
+		<header className="flex w-[250px] flex-col bg-[#4f5d75] p-12 rounded-full">
+			<Profile setModalOpen={setTransactionQRModalOpen} avatar={avatar} userAddress={userAddress} />
+			<TransactionQRModal modalOpen={transactionQRModalOpen} setModalOpen={setTransactionQRModalOpen} userAddress={userAddress} myKey={publicKey} setQrCode={setQrCode} />
 
-                <NavMenu connected={connected} publicKey={publicKey} />
+			<NavMenu connected={connected} publicKey={publicKey} />
 
-                <Action setModalOpen={setNewTransactionModalOpen} />
-                <NewTransactionModal modalOpen={newTransactionModalOpen} setModalOpen={setNewTransactionModalOpen} />
-            </header>
+			<Action setModalOpen={setNewTransactionModalOpen} />
+			<NewTransactionModal modalOpen={newTransactionModalOpen} setModalOpen={setNewTransactionModalOpen} />
+		</header>
 
-            <main className="flex flex-1 flex-col">
-                <SearchBar />
-
-                <TransactionsList connected={connected} transactions={transactions} />
-            </main>
-        </div>
-    )
+		
+	</div>
+)
 }
 
 export default Home
