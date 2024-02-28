3b67dd86cbbfcfb8f3f482b4e31c35f00a132a23
johnafekuana <afekuanajohn@gmail.com>
2024-02-28 11:41:23 +0100

    updated _app.js file

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,25 @@
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import '../styles/globals.css'
+import WalletConnectionProvider from '../context/WalletConnectionProvider'
+
 
 
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-                <title>Cash App</title>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
+	return (
+		<>
+			<Head>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
+				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+				<title>Sol Tip</title>
+			</Head>
+			<WalletConnectionProvider>
+				<Component {...pageProps} />
+			</WalletConnectionProvider>
+				
+		</>
+	)
 }
 
 export default MyApp
