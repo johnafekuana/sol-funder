083a48827d4fb2117c69e93077b23e9e0fb96e55
johnafekuana <afekuanajohn@gmail.com>
2024-02-28 11:38:16 +0100

    update TransactionsList.js file

--- frontend/components/transaction/TransactionsList.js
+++ frontend/components/transaction/TransactionsList.js
@@ -2,30 +2,38 @@ import { useMemo, useState } from 'react'
 import TransactionDetailModal from './TransactionDetailModal'
 import TransactionItem from './TransactionItem'
 
+
+const dropdownIcon = '/dropdown-icon.ico';
+
+
 const TransactionsList = ({ transactions }) => {
-    const [modalOpen, setModalOpen] = useState(false)
-    const [currentTransactionID, setCurrentTransactionID] = useState(null)
-    const currentTransaction = useMemo(() => transactions.find((transaction) => transaction.id === currentTransactionID), [currentTransactionID])
+	
+	const [modalOpen, setModalOpen] = useState(false)
+	const [currentTransactionID, setCurrentTransactionID] = useState(null)
+	const currentTransaction = useMemo(() => transactions.find((transaction) => transaction.id === currentTransactionID), [currentTransactionID])
 
-    const toggleTransactionDetailModal = (value, transactionID) => {
-        setCurrentTransactionID(transactionID)
-        setModalOpen(value)
-    }
+	const toggleTransactionDetailModal = (value, transactionID) => {
+			setCurrentTransactionID(transactionID)
+			setModalOpen(value)
+	}
 
-    return (
-        <div>
-            <div className="bg-[#f6f6f6] pb-4 pt-10">
-                <p className="mx-auto max-w-3xl px-10 text-sm font-medium uppercase text-[#abafb2] xl:px-0">Transactions</p>
-            </div>
-            <div className="mx-auto max-w-3xl divide-y divide-gray-100 py-4 px-10 xl:px-0">
-                {transactions.map(({ id, to, amount, description, transactionDate }) => (
-                    <TransactionItem key={id} id={id} to={to} description={description} transactionDate={transactionDate} amount={amount} toggleTransactionDetailModal={toggleTransactionDetailModal} />
-                ))}
+	return (
+		<div>
+			<div className="rounded-full bg-[#4f5d75] pb-4 pt-10">
+				<p className=
+						"font-poppins flex items-center justify-center pb-6 mx-auto max-w-3xl px-10 text-sm font-medium uppercase text-[#fff] xl:px-0"
+					>Recent Transactions <span className="ml-2"><img src={dropdownIcon} alt="Icon" className="w-5 h-5 animate-bounce" style={{ filter: 'invert(1)' }} /></span>
+				</p>
+			</div>
+			<div className="mx-auto max-w-3xl divide-y divide-gray-100 py-4 px-10 xl:px-0">
+				{transactions.map(({ id, to, amount, description, transactionDate }) => (
+					<TransactionItem key={id} id={id} to={to} description={description} transactionDate={transactionDate} amount={amount} toggleTransactionDetailModal={toggleTransactionDetailModal} />
+				))}
 
-                <TransactionDetailModal modalOpen={modalOpen} setModalOpen={setModalOpen} currentTransaction={currentTransaction} />
-            </div>
-        </div>
-    )
+				<TransactionDetailModal modalOpen={modalOpen} setModalOpen={setModalOpen} currentTransaction={currentTransaction} />
+			</div>
+		</div>
+	)
 }
 
 export default TransactionsList
