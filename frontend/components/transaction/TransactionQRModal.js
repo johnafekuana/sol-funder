c880fbe80d2d431b77a61c17b0f60cb45bc2d7ad
johnafekuana <afekuanajohn@gmail.com>
2024-03-14 08:32:32 +0100

    updated my Modal pop up to include an option for inputing the figure to be sent and not just hardcoded amount

--- frontend/components/transaction/TransactionQRModal.js
+++ frontend/components/transaction/TransactionQRModal.js
@@ -8,122 +8,133 @@ import { truncate } from "../../utils/string";
 import { useSolTip } from '../../hooks/soltip';
 import { getAvatarUrl } from '../../functions/getAvatarUrl';
 
-
-
-
 const TransactionQRModal = ({ modalOpen, setModalOpen, userAddress, setQrCode }) => {
-	const {transactions, setTransactions} = useSolTip()
+    const { transactions, setTransactions } = useSolTip()
 
-	const qrRef = useRef()
-	const connection = useConnection()
+    const qrRef = useRef()
+    const connection = useConnection()
 
-	// Set the state to true to rerender the component with generated QR
-	const loadQr = () => {
-		setQrCode(true)
-	}
+    const [amountValue, setAmountValue] = useState("0.0"); // State variable for the amount value
 
-	useEffect(()=>{
+    // Set the state to true to rerender the component with generated QR
+    const loadQr = () => {
+        setQrCode(true)
+    }
 
-		const recipient = new PublicKey(userAddress)
-		const amount = new BigNumber("0.1")
-		const reference = Keypair.generate().publicKey
-		const label = "My nice cookies"
-		const message = "Thank you for sending!"
+    useEffect(() => {
+        const recipient = new PublicKey(userAddress)
+        const amount = new BigNumber(amountValue) // Use amountValue from state
+        const reference = Keypair.generate().publicKey
+        const label = "My nice cookies"
+        const message = "Thank you for sending!"
 
-		const urlParams = {
-			recipient,
-			amount,
-			reference,
-			label,
-			message
-		}
-		const url = encodeURL(urlParams)
-		const qr = createQR (url, 400, 'white','#7ac1e0')
+        const urlParams = {
+            recipient,
+            amount,
+            reference,
+            label,
+            message
+        }
+        const url = encodeURL(urlParams)
+        const qr = createQR(url, 400, 'white', '#7ac1e0')
 
-		if(qrRef.current){
-			qrRef.current.innerHTML = ''
-			qr.append(qrRef.current)
-		}
+        if (qrRef.current) {
+            qrRef.current.innerHTML = ''
+            qr.append(qrRef.current)
+        }
 
-		const interval = setInterval(async()=>{
-			console.log("waiting for transaction");
-			try {
-				const signatureInfo = await findReference(connection, reference, {finality: 'confirmed'})
-				console.log("validating");
-				await validateTransfer(
-					connection,
-					signatureInfo.signature,
-					{
-						recipient,
-						amount,
-						reference,
-					},
-					{commitment: 'confirmed'}
-				)
+        const interval = setInterval(async () => {
+            console.log("waiting for transaction");
+            try {
+                const signatureInfo = await findReference(connection, reference, { finality: 'confirmed' })
+                console.log("validating");
+                await validateTransfer(
+                    connection,
+                    signatureInfo.signature,
+                    {
+                        recipient,
+                        amount,
+                        reference,
+                    },
+                    { commitment: 'confirmed' }
+                )
 
-					//add transaction to local storage
-				const newTransaction = {
-					id: transactions.length.toString(),
-					from: {
-						name: recipient,
-						handle: recipient,
-						avatar: getAvatarUrl(receiver.toString()),
-						verified: true,
-					},
-					to: {
-						name: reference,
-						handle: '-',
-						avatar: getAvatarUrl(reference.toString()),
-						verified: false,
-					},
-					description: "user sent you SOL",
-					transactionDate: new Date(),
-					status: 'Completed',
-					amount: amount,
-					source: '-',
-					identifier: '-',
-				};
-				setTransactions(prevTransactions => [newTransaction, ...prevTransactions]);
-				setModalOpen(false)
+                //add transaction to local storage
+                const newTransaction = {
+                    id: transactions.length.toString(),
+                    from: {
+                        name: recipient,
+                        handle: recipient,
+                        avatar: getAvatarUrl(receiver.toString()),
+                        verified: true,
+                    },
+                    to: {
+                        name: reference,
+                        handle: '-',
+                        avatar: getAvatarUrl(reference.toString()),
+                        verified: false,
+                    },
+                    description: "user sent you SOL",
+                    transactionDate: new Date(),
+                    status: 'Completed',
+                    amount: amount,
+                    source: '-',
+                    identifier: '-',
+                };
+                setTransactions(prevTransactions => [newTransaction, ...prevTransactions]);
+                setModalOpen(false)
 
-				clearInterval(interval)
+                clearInterval(interval)
 
-			} catch (error) {
-				if (error instanceof FindReferenceError){
-					return
-				}
-				if (error instanceof ValidateTransferError){
-					console.error('Transaction is invalid', error)
-					return 
-				}
-				console.error('Unknown error', error)
+            } catch (error) {
+                if (error instanceof FindReferenceError) {
+                    return
+                }
+                if (error instanceof ValidateTransferError) {
+                    console.error('Transaction is invalid', error)
+                    return
+                }
+                console.error('Unknown error', error)
 
-				clearInterval(interval)
-			}
-		}, 500)
+                clearInterval(interval)
+            }
+        }, 500)
 
-		return ()=> clearInterval(interval)
-	})
+        return () => clearInterval(interval)
+    }, [amountValue]) // Add amountValue to the dependency array
 
-	return (
-		<Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
-			<div >
-				<div className="flex flex-col items-center justify-center space-y-1">
-					<div ref={qrRef} />
-				</div>
+    // Function to handle amount change
+    const handleAmountChange = (event) => {
+        setAmountValue(event.target.value);
+    };
 
-				<div className="flex flex-col items-center justify-center space-y-1">
-					<p className="text-lg font-medium text-gray-800">{truncate(userAddress)}</p>
+    return (
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+            <div >
+                <div className="flex flex-col items-center justify-center space-y-1">
+                    <div ref={qrRef} />
+                </div>
 
-					<p className="text-sm font-light text-gray-600">Scan to pay ${truncate(userAddress)}</p>
+                <div className="flex flex-col items-center justify-center space-y-1">
+                    <p className="text-lg font-medium text-gray-800">{truncate(userAddress)}</p>
 
-					<button onClick={() => loadQr()} className="w-full rounded-lg bg-[#7ac1e0] py-3 hover:bg-opacity-70 outline-none">
-						<span className="font-medium text-white">Load QR code</span>
-					</button>
-				</div>
-			</div>
-		</Modal>
-	)
+                    <p className="text-sm font-light text-gray-600">Scan to pay ${truncate(userAddress)}</p>
+
+                    <input
+                        type="number"
+                        value={amountValue}
+                        onChange={handleAmountChange} // Call handleAmountChange on input change
+                        className="w-full rounded-lg bg-white py-3 outline-none text-center"
+                        placeholder="Enter amount"
+                    />
+
+                    <button onClick={() => loadQr()} className="w-full rounded-lg bg-[#7ac1e0] py-3 hover:bg-opacity-70 outline-none">
+                        <span className="font-medium text-white">Load QR code</span>
+                    </button>
+                </div>
+            </div>
+        </Modal>
+    )
 }
 
 export default TransactionQRModal
