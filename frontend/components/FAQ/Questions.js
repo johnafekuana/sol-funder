4941c0aeb37c314e8ddcba44931ac7d7caa3637c
johnafekuana <afekuanajohn@gmail.com>
2024-03-18 00:46:34 +0100

    added a FAQ section to Home Page and derived data from Questions.js file

---
+++ frontend/components/FAQ/Questions.js
@@ -0,0 +1,18 @@
+export const Questions = [
+    {
+        question: 'How does fundraising with SolFunder work?',
+        options: ["SolFunder simplifies the process of fundraising with cryptocurrency. You sign up, connect your crypto wallet, and post your profile. To contribute to a campaign, supporters enter the campaign's link and send the amount they wish to donate, and the funds are securely transferred to the campaign's wallet."]
+    },
+    {
+        question: 'Why should I use SolFunder for fundraising?',
+        options: ["SolFunder democratizes fundraising with cryptocurrency, making it accessible to all. Whether you're an individual, a nonprofit organization, or a business seeking support, SolFunder provides an easy-to-use platform for initiating and managing fundraising campaigns. It's an innovative way to engage your community and raise funds for your cause."]
+    },
+    {
+        question: 'How secure are my funds with Tipify?',
+        options: ["SolFunder prioritizes the security of your funds. We employ robust security measures, including secure wallet storage and encryption protocols, to safeguard your cryptocurrency. You retain full control over your funds, and SolFunder does not have access to your private keys, ensuring maximum security for your assets."]
+    },
+    {
+        question: 'Do I need to be knowledgeable about cryptocurrency to use SolFunder?',
+        options: ["No, you don't need to be an expert in cryptocurrency to use SolFunder. Our platform is designed to be user-friendly, with intuitive interfaces and straightforward instructions. Whether you're new to crypto or experienced, SolFunder provides a seamless experience for initiating and participating in fundraising campaigns, without requiring extensive knowledge of blockchain technology."]
+    }
+];
