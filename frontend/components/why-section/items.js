3ddd03e17171b7f639f31a06d7355d4f070e7d54
johnafekuana <afekuanajohn@gmail.com>
2024-03-17 01:57:24 +0100

    added a Why Section to our Landing Page

---
+++ frontend/components/why-section/items.js
@@ -0,0 +1,33 @@
+export const items = [
+	{
+		img: '/wallet-icon.svg',
+		alt: 'wallet icon',
+		subtitle: 'Earn more money',
+		description: 'Achieve your long-term or short-term financial goals by receiving tips through SolFunder.'
+	},
+	{
+		img: '/123-icon.svg',
+		alt: '123 number icon',
+		subtitle: 'Easy to get started',
+		description: 'Signing up, downloading your personal wallet, and connecting it is all it takes to start receiving tips with SolFunder.'
+	},
+	{
+		img: '/clock-icon.svg',
+		alt: 'clock icon',
+		subtitle: 'Choose your own hours',
+		description: 'With SolFunder, you have the flexibility to manage your fundraising efforts at any time.'
+	},
+	{
+		img: '/location-icon.svg',
+		alt: 'location icon',
+		subtitle: 'Earn from anywhere',
+		description: "SolFunder removes geographical barriers, allowing you to raise funds from any location."
+	},
+	{
+		img: '/search-icon.svg',
+		alt: 'search/transparent icon',
+		subtitle: 'Transperency',
+		description: 'With no intermediaries involved, every interaction occurs directly between you and your supporters.'
+	},
+	
+]
