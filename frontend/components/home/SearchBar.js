d776c570c41062f864b59ea25780eea311db3632
johnafekuana <afekuanajohn@gmail.com>
2024-03-19 00:47:34 +0100

    updated SearchBar.js

--- frontend/components/home/SearchBar.js
+++ frontend/components/home/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar = () => {
 		<div className="border-none px-6 py-4 font-poppins">
 			<div className="flex items-center space-x-3">
 				<MagnifyingGlassIcon className="h-5 w-5 text-[#9f9f9f]" />
-				<input className="flex-1 text-gray-600 placeholder-[#9f9f9f] outline-none" type="text" placeholder="Search for Transactions" />
+				<input className="flex-1 text-sm text-gray-600 placeholder-[#9f9f9f] outline-none" type="text" placeholder="Search for User" />
 			</div>
 		</div>
 	)
