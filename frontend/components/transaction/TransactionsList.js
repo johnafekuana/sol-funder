bb7f766d715e589334f121b10187e6765b75ea0a
johnafekuana <afekuanajohn@gmail.com>
2024-03-19 00:50:01 +0100

    updated Transaction List UI at TransactionList.js

--- frontend/components/transaction/TransactionsList.js
+++ frontend/components/transaction/TransactionsList.js
@@ -19,13 +19,23 @@ const TransactionsList = ({ transactions }) => {
 
 	return (
 		<div>
-			<div className="rounded-full bg-[#4f5d75] pb-4 pt-10">
+			<div className="rounded-full bg-[#2563EB] pb-4 pt-4 mx-2">
 				<p className=
-						"font-poppins flex items-center justify-center pb-6 mx-auto max-w-3xl px-10 text-sm font-medium uppercase text-[#fff] xl:px-0"
-					>Recent Transactions <span className="ml-2"><img src={dropdownIcon} alt="Icon" className="w-5 h-5 animate-bounce" style={{ filter: 'invert(1)' }} /></span>
+						"font-poppins flex items-center justify-center mx-auto max-w-3xl px-10 text-sm font-medium uppercase text-[#fff] xl:px-0"
+					>Donations <span className="ml-2"><img src={dropdownIcon} alt="Icon" className="w-5 h-5" style={{ filter: 'invert(1)' }} /></span>
 				</p>
 			</div>
-			<div className="mx-auto max-w-3xl divide-y divide-gray-100 py-4 px-10 xl:px-0 bg-gray-100 animate-pulse-bg mt-2 rounded-lg">
+
+			<div className='campaign-description text-center text-xs  mx-80 rounded-lg mt-2 bg-[#fff] text-[#2563EB] p-4'>
+
+				<p className='animate-bounce'>
+					Hey guys, I am currently raising funds for my rent. <br/>
+					Thank you.
+				</p>
+
+			</div>
+
+			<div className="mx-auto max-w-3xl divide-y divide-gray-100 py-4 px-10 xl:px-0 bg-gray-100 mt-2 rounded-lg animate-pulse-bg">
 				{transactions.map(({ id, to, amount, description, transactionDate }) => (
 					<TransactionItem key={id} id={id} to={to} description={description} transactionDate={transactionDate} amount={amount} toggleTransactionDetailModal={toggleTransactionDetailModal} />
 				))}
